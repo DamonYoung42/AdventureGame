c158fe20e2c4b5bd60f5dd86e02099c11bcbafe2
DamonYoung42 <damonyoung@wi.rr.com>
2016-08-09 15:55:25 -0500

    revamped adventure game

---
+++ AdventureGame/Game.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Game
+    {
+        Player playerOne;
+        int numberOfTasks;
+        bool finishedTasks;
+
+
+        public Game()
+        {
+            this.playerOne = new Player(this.EnterPlayerName());
+            this.numberOfTasks = 3;
+            this.finishedTasks = false;
+        }
+        
+
+        public void RunGame()
+        {
+
+            Console.WriteLine("If you accept the challenge, you will attempt to complete 3 random tasks. If you complete these tasks, you win!");
+            Console.WriteLine("Are you ready to start? (Y/N):");
+
+            if (Console.ReadLine() == "n")
+            {
+                EndGame();
+            }
+
+            else
+            {
+                PlayGame();
+                EndGame();
+            }
+
+        }
+    
+
+        public string EnterPlayerName()
+        {
+            Console.WriteLine("Enter your player name:");
+            return Console.ReadLine();
+        }
+
+        public void EndGame()
+        {
+            if (this.finishedTasks)
+            {
+                WinMessage();
+            }
+            else
+            {
+                LoseMessage();
+            }
+
+            if (RetryGame())
+            {
+                this.playerOne.SetTasks();
+                RunGame();
+            }
+            else
+            {
+                Console.WriteLine("See you next time, {0}!", this.playerOne.getName());
+            }
+
+        }
+
+        public void LoseMessage()
+        {
+            Console.WriteLine("Sorry, you did not complete 3 tasks.");
+        }
+
+        public void WinMessage()
+        {
+            Console.WriteLine("Awesome, you won the game!");
+        }
+
+        public void PlayGame()
+        {
+             Console.WriteLine("Welcome, {0}!", this.playerOne.getName());
+
+            for (int i = 0; i < this.numberOfTasks; i++)
+            {
+                this.finishedTasks = this.playerOne.CompleteTask(this.playerOne.ChooseTask());
+                if (this.finishedTasks == false)
+                {
+                    break;
+                }
+            }
+        }
+
+        public bool RetryGame()
+        {
+            Console.WriteLine("Would you like to play again? Y/N:");
+            if (Console.ReadLine() == "y")
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ AdventureGame/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Player
+    {
+        string playerName;
+        List<string> taskList;
+        int taskNumber;
+
+        public Player(string name)
+        {
+            this.playerName = name;
+            this.taskList = new List<string> { "Steal a candy bar", "Hit 100 mph on a Harley", "Dive off the Hoan Bridge", "Skinny dip at Bradford Beach", "Hang from the Miller Park roof for 10 seconds" };
+        }
+
+
+        public string getName()
+        {
+            return this.playerName;
+        }
+
+        public bool CompleteTask(int taskNumber)
+        {
+            this.taskList.RemoveAt(taskNumber);
+            Random random = new Random(DateTime.Now.Millisecond);
+            double randomNumber = random.NextDouble();
+
+            if (randomNumber >= 0.5)
+            {
+                Console.WriteLine("Result: You did it!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Result: You failed");
+                return false;
+            }
+
+        }
+
+        public int ChooseTask()
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            this.taskNumber = random.Next(0, this.taskList.Count());
+            Console.WriteLine("Task: {0}", this.taskList[this.taskNumber]);
+            return this.taskNumber;
+        }
+
+        public void SetTasks()
+        {
+            this.taskList = new List<string> { "Steal a candy bar", "Hit 100 mph on a Harley", "Dive off the Hoan Bridge", "Skinny dip at Bradford Beach", "Hang from the Miller Park roof for 10 seconds"};
+        }
+
+    }
+}
